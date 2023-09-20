a3515648da49e51d72d0b4a28b9446b8895f9422
vita0419 <vita.ynwa.0419@gmail.com>
2023-09-20 16:13:24 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,27 @@
 let express = require('express');
 let app = express();
+
+//4
+const staticPath = __dirname + "/public";
+app.use(express.static(staticPath));
+
+//2
 app.get("/", (req, res) => {
- res.send("Hello Express");
+    res.send("Hello Express");
 })
+
+//3
+app.get("/", (req, res) => {
+    const indexPath = __dirname + "/views/index.html";
+    res.sendFile(indexPath);
+})
+
+//5
+app.get("/json", (req, res) => {
+    res.json({"message": "Hello json"});
+})
+
+//1
 app.all("*", (req, res) => {
  //console.log("Hello World");
 })
